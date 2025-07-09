daf988a3a7f8bcd86ba3a346d966604709dd3754
adi2405 <144616497+adi2405@users.noreply.github.com>
2025-07-10 00:52:12 +0530

    server sidebar ui completed

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,46 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ serverId: string }>;
+}) => {
+  const profile = await currentProfile();
+  const { serverId } = await params;
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex flex-col fixed h-full w-60 z-20 inset-y-0">
+        <ServerSidebar serverId={serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
